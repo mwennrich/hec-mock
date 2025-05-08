c53643728a77dba2c57738667d00c36d70ea3b28
Markus Wennrich <markus.wennrich@f-i-ts.de>
2025-05-08 07:01:35 +0200

    linter fixes

--- main.go
+++ main.go
@@ -1,37 +1,50 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io"
-    "log"
-    "net/http"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"time"
 )
 
 func hecHandler(w http.ResponseWriter, r *http.Request) {
-    body, err := io.ReadAll(r.Body)
-    if err != nil {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
 		log.Println("Error reading body:", err)
-        http.Error(w, "can't read body %w", http.StatusBadRequest)
-        return
-    }
+		http.Error(w, "can't read body %w", http.StatusBadRequest)
+		return
+	}
 
-    fmt.Println(string(body))
+	fmt.Println(string(body))
 
-    resp := map[string]interface{}{
-        "text":  "Success",
-        "code":  0,
-        "ackId": 1234,
-    }
+	resp := map[string]interface{}{
+		"text":  "Success",
+		"code":  0,
+		"ackId": 1234,
+	}
 
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(resp)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(resp)
+	if err != nil {
+		log.Println("Error encoding response:", err)
+		http.Error(w, "can't encode response %w", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/services/collector/event", hecHandler)
-    mux.HandleFunc("/services/collector", hecHandler)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/services/collector/event", hecHandler)
+	mux.HandleFunc("/services/collector", hecHandler)
 
-    log.Fatal(http.ListenAndServe(":8080", mux))
+	server := &http.Server{
+		Addr:              ":8080",
+		ReadHeaderTimeout: 1 * time.Minute,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("Error starting server:", err)
+	}
 }
